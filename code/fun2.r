af65c235b33d4ccc410e469c56f26b7448efefbb
Chenkai_Zhao <ck_zhao@163.com>
2024-01-12 15:38:34 +0800

    Function of ShinyApp

--- code/fun2.r
+++ code/fun2.r
@@ -114,7 +114,7 @@ Plot.Stream.JS <- function(dat, FctLevelJS){
   axis_break <- seq.Date(min(dat$SampelDate), max(dat$SampelDate), by = "4 week") 
   
   datLabel <- dat[week == max(week), .SD
-  ][, ":="(PercLabel = paste0(Label, " (", sprintf("%.2f", PercWeek * 100), "%)"),
+  ][, ":="(PercLabel = paste0(Label, " (", sprintf("%.1f", PercWeek * 100), "%)"),
            var3 = factor(Label, levels = FctLevelJS), Date = max(dat$SampelDate))
   ][order(-PercWeek)][1:5][, index := rev(1:length(Label)) * 0.1]
   
@@ -122,10 +122,10 @@ Plot.Stream.JS <- function(dat, FctLevelJS){
   
   fig1 <- ggplot(dat) +
     geom_stream(aes(x = SampelDate, y = PercWeek, fill = Label), type = "proportional") +
-    geom_label(data = datLabel, mapping = aes(x = Date + 5, y = index, label = PercLabel),
-               color = "black", fill = "#FCFDBFFF", label.size = NA, size = 8, hjust = 0) +
+    geom_label(data = datLabel, mapping = aes(x = Date + 2, y = index, label = PercLabel),
+               color = "black", fill = "#FCFDBFFF", label.size = NA, size = 4, hjust = 0) +
     scale_fill_manual(values = fill_color) +
-    scale_x_date(expand = expansion(mult = c(0.1, 0.3)), breaks = axis_break, date_labels = "%b %Y") +
+    scale_x_date(expand = expansion(mult = c(0.01, 0.3)), breaks = axis_break, date_labels = "%Y-%m") +
     scale_y_continuous(
       expand = c(0, 0), labels = scales::percent_format(scale = 100), breaks = seq(0, 1, 0.1),
       sec.axis = sec_axis(trans = ~., labels = scales::percent_format(scale = 100), breaks = seq(0, 1, 0.1))
@@ -139,17 +139,19 @@ Plot.Stream.JS <- function(dat, FctLevelJS){
           legend.key.height = unit(0.3, "cm"),
           legend.key.width = unit(0.7, "cm"),
           legend.text = element_text(size = 14),
-          plot.title = element_text(hjust = 1, face = "bold"),
-          plot.subtitle = element_text(hjust = 1, face = "bold"),
+          legend.title = element_text(size = 14, face = "bold"),
+          plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(hjust = 0, face = "bold"),
           plot.caption = element_text(hjust = 0),
           plot.margin = margin(10, 10, 10, 10)) + 
-    labs(title = "Timecourse of SARS-Cov-2 Variants sublineage distribution",
-         subtitle = paste0(min(dat$SampelDate), " — ", max(dat$SampelDate)))
+    labs(title = NULL, # "新冠病毒亚型分布变化",
+         x = NULL, y = NULL, fill = "病毒亚型",
+         subtitle = paste0("报告时间：", min(dat$SampelDate), " — ", max(dat$SampelDate)))
   
   
   fig2 <- ggplot(dat) +
     geom_line(aes(x = SampelDate, y = PercWeek, color = Label), linewidth = 1) +
-    scale_x_date(expand = c(0.01, 0.5), breaks = axis_break, date_labels = "%b %Y") +
+    scale_x_date(expand = c(0.01, 0.5), breaks = axis_break, date_labels = "%Y-%m") +
     scale_y_continuous(expand = c(0, 0)) +
     theme_classic() +
     scale_color_manual(values = fill_color) +
@@ -160,24 +162,26 @@ Plot.Stream.JS <- function(dat, FctLevelJS){
           legend.key.height = unit(0.3, "cm"),
           legend.key.width = unit(0.7, "cm"),
           legend.text = element_text(size = 14),
+          legend.title = element_text(size = 14, face = "bold"),
           plot.title = element_text(hjust = 1, face = "bold"),
           plot.subtitle = element_text(hjust = 1, face = "bold"),
           plot.caption = element_text(hjust = 0),
           plot.margin = margin(10, 10, 10, 10)) + 
     labs(title = "Timecourse of SARS-Cov-2 Variants sublineage distribution",
-         subtitle = paste0(min(dat$SampelDate), " — ", max(dat$SampelDate)))
+         x = NULL, y = NULL, color = "病毒亚型",
+         subtitle = paste0("报告时间：", min(dat$SampelDate), " — ", max(dat$SampelDate)))
 
   
   MaxDay <- dat[, .SD[which.max(PercWeek)], by = .(Label)
   ][, week := ISOweek2date(paste0(year(SampelDate), "-W", 
                                   sprintf("%02d", week), "-1")) 
     # %d表示目标数据是一个整数，02：这部分指定了数字的最小宽度为2位。如果数字本身不足两位，那么会在数字前面补充0。
-  ][, PercWeek := PercWeek * 100
+  ][, PercWeek := round((PercWeek * 100), 1)
   ][, -c("SampelDate")
   ][order(-PercWeek)
-  ][, .(Label, week, TotalVarWeek, TotalWeek, PercWeek)]
-  colnames(MaxDay) <- c("Virus type", "Date", "Reported cases", "Total cases", "Max proportion")
-  
+  ][, PercWeek := paste0(PercWeek, "%")
+  ][, .(Label, week, PercWeek, TotalVarWeek, TotalWeek)]
+  colnames(MaxDay) <- c("病毒亚型", "流行峰值时间", "亚型占比", "亚型报告病例数", "累积病例数")
   print(fig1)
   print(fig2)
   
@@ -191,3 +195,7 @@ Text.Date <- function(dat){
          year(MaxDate), "年", month(MaxDate), "月", day(MaxDate), "日，")
 }
 
+Text.Date2 <- function(dat){
+  Date <- ymd(dat)
+  paste0(year(Date), "年", month(Date), "月", day(Date), "日")
+}
